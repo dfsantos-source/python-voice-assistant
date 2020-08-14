9748c19ed604f91c80c0489bc5df597aa7db7023
Dane Santos <dsantos@umass.edu>
2020-08-13 21:17:15 -0400

    added speech-to-text functionality, begun google calendar api implementation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import playsound
+import pyaudio
 import speech_recognition as sr
 from gtts import gTTS
 
@@ -10,4 +11,27 @@ def speak(text):
     tts.save(filename)
     playsound.playsound(filename)
 
-speak("hi")
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source=source,timeout=5,phrase_time_limit=5)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except LookupError:
+            print("Could not understand")
+     
+    return said
+
+
+text = get_audio()
+
+if "hello" in text:
+    speak("hello, how are you?")
+
+if "what is your name" in text:
+    speak("my name is Sofia")
+
+
